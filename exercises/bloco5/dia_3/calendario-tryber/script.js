3b8a84f639b7866899d300c066d8ad1e3943c388
Gustavo Zanette <gmzo.gustavo@gmail.com>
2021-04-07 20:09:55 -0300

    exercicio 1

--- exercises/bloco5/dia_3/calendario-tryber/script.js
+++ exercises/bloco5/dia_3/calendario-tryber/script.js
@@ -22,3 +22,66 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+const dezDaysList = [
+	29,
+	30,
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+	10,
+	11,
+	12,
+	13,
+	14,
+	15,
+	16,
+	17,
+	18,
+	19,
+	20,
+	21,
+	22,
+	23,
+	24,
+	25,
+	26,
+	27,
+	28,
+	29,
+	30,
+	31,
+];
+
+// Thinking proccess
+
+// função que criará elementos li para cada dia do array dezDaysList como filho da ul com id #days
+// forof percorrendo array dezDaysList
+//    1- variavel day armazena a criacao de novo elemento html 'li'
+//    2- se for feriado: aplica classe holiday, se for sexta: aplica classe friday
+//    3 - aplica classe day
+//    4 - busca ul com id #days e aplica appendChild nele levendo day como parametro
+function createDays() {
+	for (const days of dezDaysList) {
+		var day = document.createElement('li');
+		day.innerText = days;
+		day.className = 'day';
+
+		if (days === 24 || days === 25 || days === 31) {
+			day.classList.add('holiday');
+		}
+
+		if (days === 4 || days === 11 || days === 18 || days === 25) {
+			day.classList.add('friday');
+		}
+
+		document.querySelector('#days').appendChild(day);
+	}
+}
+
+createDays();
